5ecda8822091d47f7d6ca0fb07173792b6b491e5
Sebastian Mai <sebastian.mai@ovgu.de>
2022-03-24 16:31:38 +0100

    adding more bechmarking

--- polygonal_roadmaps/__main__.py
+++ polygonal_roadmaps/__main__.py
@@ -9,48 +9,49 @@ from .polygonal_roadmap import CBSPlanner, Executor, MapfInfoEnvironment
 from .polygonal_roadmap import pathfinding
 
 
-def run_all(config):
-    assert config['planner'] in ['CBS']
-    for scen in config['env']['scen']:
-        env = MapfInfoEnvironment(scen, n_agents=config['env']['n_agents'])
-        run_one(config, env)
+def run_all(args):
+    for planner in args.planner:
+        for scenario in args.scen:
+            run_scenarios(scenario, planner)
 
 
-def run_one(config, env):
+def create_planner_from_config(config, env):
     if config['planner'] == 'CBS':
-        planner = CBSPlanner(env, **config['planner_args'])
-    else:
-        logging.warn(f"Planner '{config['planner']}' not found")
-        return
-    ex = Executor(env, planner)
+        return CBSPlanner(env, **config['planner_args'])
+    raise NotImplementedError(f"planner {config['planner']} does not exist.")
+
+
+def run_scenarios(scenario_yml, planner_yml):
+    with open(Path("benchmark") / 'planner_config' / planner_yml) as stream:
+        planner_config = yaml.safe_load(stream)
+    with open(Path("benchmark") / 'scenario_config' / scenario_yml) as stream:
+        scenario_config = yaml.safe_load(stream)
+    for scen in scenario_config['scen']:
+        env = MapfInfoEnvironment(scen, n_agents=scenario_config['n_agents'])
+
+        planner = create_planner_from_config(planner_config, env)
+        path = Path('results') / planner_yml / scenario_yml
+        path.mkdir(parents=True, exist_ok=True)
+        run_one(planner, result_path=path / 'result.pkl')
+
+
+def run_one(planner, result_path=None):
+    ex = Executor(planner.env, planner)
     ex.run()
     print(f'n_agents={len(ex.history[0])}')
     print(f'took {len(ex.history)} steps to completion')
     print(f'k-robustness with k={pathfinding.compute_solution_robustness(ex.get_history_as_solution())}')
 
     logging.info('done')
-    if config['results_location'] is not None:
-        path = Path(f'{config["results_location"]}') / config['config_file'] / env.scenario_file
-        path.mkdir(parents=True, exist_ok=True)
-        with open(path / 'result.pkl', mode="wb") as results:
+    if result_path is not None:
+        with open(result_path, mode="wb") as results:
             pickle.dump(ex.get_result(), results)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Runner for Pathfinding Experiments")
-    parser.add_argument("-config", type=str, required=True)
-    parser.add_argument("-scen", type=str, nargs='+', default=None)
+    parser.add_argument("-planner", type=str, nargs='+', required=True)
+    parser.add_argument("-scen", type=str, nargs='+', required=True)
     parser.add_argument("-n_agents", type=int, default=None)
-    parser.add_argument("-results_location", type=str, default=None)
     args = parser.parse_args()
-    with open(Path("benchmark") / 'config' / args.config) as stream:
-        config = yaml.safe_load(stream)
-    if args.scen is not None:
-        config["env"]["scen"] = args.scen
-    if args.n_agents is not None:
-        config["env"]["n_agents"] = args.n_agents
-    config["results_location"] = args.results_location
-    if config["results_location"] is None:
-        config["results_location"] = "results/"
-    config['config_file'] = args.config
-    run_all(config)
+    run_all(args)
