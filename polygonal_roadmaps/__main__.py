733c6ab603694d2d0dcc913c063bd153caf1b69e
Sebastian Mai <sebastian.mai@ovgu.de>
2022-04-08 15:40:49 +0200

    use 10 agents as default

--- polygonal_roadmaps/__main__.py
+++ polygonal_roadmaps/__main__.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     parser.add_argument("-planner", type=str, nargs='+', required=True)
     parser.add_argument("-scentype", type=str, default="even")
     parser.add_argument("-maps", type=str, nargs='+', required=True)
-    parser.add_argument("-n_agents", type=int, default=None)
+    parser.add_argument("-n_agents", type=int, default=10)
     parser.add_argument("-index", type=int, default=None)
     parser.add_argument("-loglevel", type=str, default=None)
     parser.add_argument("-logfile", type=str, default=None)
