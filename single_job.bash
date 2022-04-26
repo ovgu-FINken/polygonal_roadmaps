fb172deddcaa55472fa060a1d125539bb8538eb6
Sebastian Mai <sebastian.mai@ovgu.de>
2022-04-26 13:06:20 +0200

    changing script runners

--- single_job.bash
+++ single_job.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --time=10:00
+#SBATCH --time=1:00:00
 # Make all cores available to our task
 #SBATCH --cpus-per-task=1
 # Use all partition
 #SBATCH --partition=ci
+#SBATCH --mem-per-cpu=4Gb
 # Redirect output and error output
-#SBATCH --output="logs/slurm-%A-%a.out"
-#SBATCH --error="logs/slurm-%A-%a.err"
+#SBATCH --output="logs/slurm.out"
+#SBATCH --error="logs/slurm.err"
 MAP=$2
 PLANNER=$1
 source bin/activate
-srun python -m polygonal_roadmaps -maps $MAP -planner $PLANNER -index $SLURM_ARRAY_TASK_ID -logfile "logs/$PLANNER$MAP/$SLURM_ARRAY_TASK_ID.log" -loglevel warning
+srun python -m polygonal_roadmaps -maps $MAP -planner $PLANNER -index $SLURM_ARRAY_TASK_ID -logfile "logs/$PLANNER$MAP/$SLURM_ARRAY_TASK_ID.log" -loglevel warning -memlimit 2 -timelimit 5
